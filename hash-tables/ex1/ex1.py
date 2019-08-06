84edc8fc2d9916958bc6797f8d98b0e14fbc5703
Robert Billington <chillingtonbillington@gmail.com>
2019-08-06 10:27:58 -0500

    Ex 1 completed

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -9,12 +9,24 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
 
-    return None
+    if length < 2:
+        answer = None
 
+    for i in range(length)
+        hash_table_insert(ht, weights[i], i)
+
+    for i in range(length)
+        weight_1 = weights[i]
+        weight_2 = limit - weight_1
+        retrieve = hash_table_retrieve(ht, weight_2)
+
+        if retrieve is not None:
+            if retrieve > i:
+                answer = (retrieve, i)
+            if retrieve < i:
+                answer = (i, retrieve)
+    return answer 
 
 def print_answer(answer):
     if answer is not None:
